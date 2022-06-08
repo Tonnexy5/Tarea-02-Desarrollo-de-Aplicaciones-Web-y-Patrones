b90748c6d709db9ecffe870d7bcfacd3a453cb70
antho <antho@TonnexyAsusTUF>
2022-06-07 21:13:45 -0600

    TArea 2 PAgina web

---
+++ src/main/java/com/tarea2tonnexy5/demo/TonnexyTubeApplication.java
@@ -0,0 +1,13 @@
+package com.tarea2tonnexy5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonnexyTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonnexyTubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea2tonnexy5/demo/TonnexyTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2tonnexy5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonnexyTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
